25874e7f2238f51827c7eeb3256251cc34c7db39
FaruqHZ <81447857+FaruqHZ@users.noreply.github.com>
2022-11-20 23:49:06 +0700

    Update Murajaah.java

--- Murajaah.java
+++ Murajaah.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
+// nama public class sesuai dengan nama file java yang kita buat
 public class Murajaah {
 
     public static void main(String[] args) {
 
         Random ayatRandom = new Random();
 
-// Jangkauan nomor yang diacak yaitu (0-140)
+// jangkauan nomor yang diacak yaitu (0-140)
         int n = ayatRandom.nextInt(141);
 
-// Jika ingin mendapatkan jangkauan yang sesuai dengan angka yang dimasukkan di class random
-// Maka variabel n harus ditambah dengan 1
+// jika ingin mendapatkan jangkauan yang sesuai dengan angka yang dimasukkan di class random
+// maka variabel n harus ditambah dengan 1
         n += 1;
 
-// Sebelum n ditambah 1, maka jangkauan angka yang diacak adalah dari 0-140
-// Setelah n ditambah 1, maka jangkauan angka yang diacak adalah dari 1-141
+// sebelum n ditambah 1, maka jangkauan angka yang diacak adalah dari 0-140
+// setelah n ditambah 1, maka jangkauan angka yang diacak adalah dari 1-141
 
         System.out.println("Bacakan surah Al-Baqarah ayat " + n);
 
     }
-
 }
