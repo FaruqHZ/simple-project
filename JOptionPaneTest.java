d732e8160d72b5ede710ca1347513a921edf14f6
Muhammad Faruq HZ <81447857+FaruqHZ@users.noreply.github.com>
2022-11-29 20:22:53 +0700

    Update JOptionPaneTest.java

--- JOptionPaneTest.java
+++ JOptionPaneTest.java
@@ -27,7 +27,7 @@ public class JOptionTest {
 // dengan cara
 // 'jenis tipe data' + dilanjutkan dengan titik '.' 'Parse+jenis tipe data' baru dimasukkan kodingan '(JOptionPane.showInputDialog("..."));'
 
-// kodingan untuk me-parse-kan variabel yang tipe datanya 'int' berbeda dengan kodingan untuk meng-parse-kan variabel yang tipe datanya 'double'
+// kodingan untuk me-parse-kan variabel yang tipe datanya 'int' berbeda dengan kodingan untuk me-parse-kan variabel yang tipe datanya 'double'
 // perbedaan kodingannya hanya terletak pada jenis tipe datanya saja
 
         nama = JOptionPane.showInputDialog("Masukkan Nama : "); // ini untuk yang bertipe data 'String' maka kodingannya langsung 'JOptionPane.showInputDialog("...");'
